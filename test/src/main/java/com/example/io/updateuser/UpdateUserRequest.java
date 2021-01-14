217d114cf105be0030d61099111c823c699d52c2
XuanXinUV <793342956@qq.com>
2021-01-14 17:17:36 +0800

    feat

---
+++ test/src/main/java/com/example/io/updateuser/UpdateUserRequest.java
@@ -0,0 +1,12 @@
+package com.example.io.updateuser;
+
+import lombok.Data;
+
+@Data
+public class UpdateUserRequest {
+    private String id;
+    private String userName;
+    private String passWord;
+    private String realName;
+    private String phone;
+}
---
+++ test/src/main/java/com/example/io/updateuser/UpdateUserResponse.java
@@ -0,0 +1,8 @@
+package com.example.io.updateuser;
+
+import lombok.Data;
+
+@Data
+public class UpdateUserResponse {
+    private Boolean result;
+}
